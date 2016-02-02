a72febdcf242280143ec8b4c054d6b268fc65016
Momchil Tomov <momchil@memsql.com>
2016-02-02 12:57:35 -0800

    database url like a normal person
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Maniphest Tasks:

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from constants import DatabaseConstants
+import os
+
 from models import db
 
 # start Flask app
@@ -10,7 +11,9 @@ app = Flask(__name__)
 
 # configure database
 #
-app.config['SQLALCHEMY_DATABASE_URI'] = DatabaseConstants.DATABASE_URI
+#DATABASE_URL = 'mysql://root@127.0.0.1:3307/db?charset=utf8&init_command=set%%20character%%20set%%20utf8' # for local testing
+DATABASE_URL = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db.init_app(app)
 
 from app import views
--- app/constants.py
+++
@@ -1,6 +0,0 @@
-class DatabaseConstants:
-    DATABASE_URI_TEMPLATE = "mysql://root:wtf@blabla.rds.amazonaws.com/%s?charset=utf8&init_command=set%%20character%%20set%%20utf8" # TODO CHANGE AND PUT IN ENV VARIABLE
-    DATABASE_LOCAL_URI_TEMPLATE = "mysql://root@127.0.0.1:3307/%s?charset=utf8&init_command=set%%20character%%20set%%20utf8" # TODO CHANGE
-    DATABASE_NAME = 'db' # TODO CHANGE
-    DATABASE_URI = DATABASE_LOCAL_URI_TEMPLATE % DATABASE_NAME # TODO CHANGE
-    DATABASE_LOCAL_URI = DATABASE_LOCAL_URI_TEMPLATE % DATABASE_NAME
